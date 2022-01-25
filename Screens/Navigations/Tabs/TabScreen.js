1231b8482f05489ef9203ca9641e3faf683040b4
AJITH <ajith@AJITHs-Mac-Pro.local>
2022-01-25 09:35:59 +0530

    adding icons

--- Screens/Navigations/Tabs/TabScreen.js
+++ Screens/Navigations/Tabs/TabScreen.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Dashboard from './Dashboard';
 import Search from './Search';
 import Cart from './Cart';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const TabScreen = () => {
   const Tabs = createBottomTabNavigator();
@@ -13,7 +14,12 @@ const TabScreen = () => {
       <Tabs.Screen
         component={Dashboard}
         name="Dashboard"
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            return <Icon name="rocket" size={20} color="black" />;
+          },
+        }}
       ></Tabs.Screen>
       <Tabs.Screen component={Search} name="Search"></Tabs.Screen>
       <Tabs.Screen component={Cart} name="Cart"></Tabs.Screen>
